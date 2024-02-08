8e6c8761c4f2738f476dc7a54a841dd39749ce3d
sulumor <romuald.patfoort@gmail.com>
2024-02-08 13:53:22 +0100

    feat: add error 404 page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from '@/pages/App/App.tsx'
 import Test from '@/pages/test/test.tsx'
+import ErrorPage from "@/pages/Error/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>
+    element: <App/>,
+    errorElement: <ErrorPage/>
   },
   {
     path: "/test",
