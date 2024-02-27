501ccc8252dba2bf73e7f5ca19acde3ec782269b
AlexisDelabaere <alexis.delabaere@gmail.com>
2024-02-27 14:48:17 +0100

    fix: multiple fixes and enhancement

--- src/components/Scout/ScoutInfos/ScoutInfos.tsx
+++ src/components/Scout/ScoutInfos/ScoutInfos.tsx
@@ -1,32 +1,16 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import {
   Text,
-  IconButton,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  FormControl,
-  FormLabel,
-  Button,
-  Input,
-  useDisclosure,
   Flex,
   Avatar,
   Box,
 } from "@chakra-ui/react";
-import { EditIcon } from "@chakra-ui/icons";
 import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "@/components/hooks/redux";
 import { getScoutInfos } from "@/components/store/reducers/scout";
-import crud from "@/utils/crud";
-import { scoutUpdate } from "@/components/store/actions/scout";
+import UpdateScoutButton  from "@/components/Button/updateScoutProfil"
 
 const ScoutInfos = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
   const id = localStorage.getItem("id");
   const dispatch = useAppDispatch();
 
@@ -36,13 +20,13 @@ const ScoutInfos = () => {
   const city = useAppSelector((state) => state.scout.city);
   const count = useAppSelector((state) => state.scout.count);
 
-  const [patchValues, setPatchValues] = useState({
+   const [patchValues, setPatchValues] = useState({
     firstname: "",
     lastname: "",
     email: "",
     club: "",
     city: "",
-  });
+  }); 
 
   useEffect(() => {
     const fetchData = async () => {
@@ -56,24 +40,6 @@ const ScoutInfos = () => {
     };
     fetchData();
   }, [count]);
-
-  const handleChangeField =
-    (scout: "firstname" | "lastname" | "email" | "club" | "city") =>
-    (value: string) => {
-      setPatchValues({ ...patchValues, [scout]: value });
-    };
-
-  const handleSubmit = () => {
-    updateScoutInfos();
-  };
-
-  const updateScoutInfos = async () => {
-    const response = await crud.update(["scout"], [Number.parseInt(id!, 10)], {
-      ...patchValues,
-    });
-    dispatch(scoutUpdate());
-    return response.data;
-  };
 
   return (
         <div className="sprofil_container">
@@ -93,87 +59,10 @@ const ScoutInfos = () => {
                   <Text fontSize="xl">Ville: {city}</Text>
                   <Text fontSize="xl">Club: {club}</Text>
                 </div>
-                <IconButton
-                  onClick={onOpen}
-                  colorScheme="teal"
-                  aria-label="Search database"
-                  icon={<EditIcon />}
-                />
+                <UpdateScoutButton />
               </div>
             </Box>
           </Flex>
-          <Modal isOpen={isOpen} onClose={onClose}>
-            <form onSubmit={handleSubmit}>
-              <ModalOverlay />
-              <ModalContent>
-                <ModalHeader>Modifiez vos informations</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody pb={6}>
-                  <FormControl>
-                    <FormLabel>Prénom</FormLabel>
-                    <Input
-                      value={patchValues.firstname}
-                      onChange={(e) =>
-                        handleChangeField("firstname")(e.target.value)
-                      }
-                    />
-                  </FormControl>
-
-                  <FormControl mt={4}>
-                    <FormLabel>Nom</FormLabel>
-                    <Input
-                      value={patchValues.lastname}
-                      onChange={(e) =>
-                        handleChangeField("lastname")(e.target.value)
-                      }
-                    />
-                  </FormControl>
-
-                  <FormControl mt={4}>
-                    <FormLabel>Email</FormLabel>
-                    <Input
-                      value={patchValues.email}
-                      onChange={(e) =>
-                        handleChangeField("email")(e.target.value)
-                      }
-                    />
-                  </FormControl>
-
-                  <FormControl mt={4}>
-                    <FormLabel>Ville</FormLabel>
-                    <Input
-                      value={patchValues.city}
-                      onChange={(e) =>
-                        handleChangeField("city")(e.target.value)
-                      }
-                    />
-                  </FormControl>
-
-                  <FormControl mt={4}>
-                    <FormLabel>Club</FormLabel>
-                    <Input
-                      value={patchValues.club}
-                      onChange={(e) =>
-                        handleChangeField("club")(e.target.value)
-                      }
-                    />
-                  </FormControl>
-
-                  <FormControl mt={4}>
-                    <FormLabel>Photo</FormLabel>
-                    <Input size="md" type="file" />
-                  </FormControl>
-                </ModalBody>
-
-                <ModalFooter>
-                  <Button onClick={handleSubmit} colorScheme="teal" mr={3}>
-                    Modifier
-                  </Button>
-                  <Button onClick={onClose}>Annuler</Button>
-                </ModalFooter>
-              </ModalContent>
-            </form>
-          </Modal>
         </div>
   );
 };
