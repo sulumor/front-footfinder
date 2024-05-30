f136f35e2692802229d1686a7ebbb51c5cd2c1cb
sulumor <romuald.patfoort@gmail.com>
2024-05-30 14:44:36 +0200

    refacto: refacto notices page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,11 +5,8 @@ import ErrorPage from "@/pages/Error/Error";
 import { HomeScout, Search } from "@/pages/Scout";
 import { HomePlayer, Match }  from "@/pages/Player";
 import { Signup, Login } from "@/pages/Auth";
-import SigninPlayer from "@/pages/Signin/SigninPlayer";
-import SigninScout from "@/pages/Signin/SigninScout";
-import NoticesPage from "@/pages/Notices/NoticesPage";
 import { GuestLayout, PlayerLayout, ScoutLayout } from "@/components/Layout";
-import { Profil } from "@/pages/Common";
+import { Profil, Notices } from "@/pages/Common";
 
 export const router = createBrowserRouter([
   {
@@ -20,7 +17,7 @@ export const router = createBrowserRouter([
       { index: true, element: <Home /> },
       { path: "/login", element: <Login /> },
       { path: "/signup", element: <Signup /> },
-      { path: "/notices", element: <NoticesPage /> },
+      { path: "/notices", element: <Notices /> },
 
     ],
   },
@@ -31,7 +28,6 @@ export const router = createBrowserRouter([
       { index: true, element: <HomePlayer /> },
       { path: "/player/me", element: <Profil /> },
       { path: "/player/match", element: <Match /> },
-      // { path: "/player/create", element: <SigninPlayer /> },
     ],
   },
   {
@@ -41,7 +37,6 @@ export const router = createBrowserRouter([
       { index: true, element: <HomeScout /> },
       { path: "/scout/me", element: <Profil /> },
       { path: "/scout/search", element: <Search/> },
-      // { path: "/scout/create", element: <SigninScout /> },
     ],
   },
 ]);
