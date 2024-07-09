b189fc4eea8f41bb58080782c9d003be43d4531b
sulumor <romuald.patfoort@gmail.com>
2024-07-09 16:16:57 +0200

    feat : popover with test Login

---
+++ src/components/Popover/Test.tsx
@@ -0,0 +1,25 @@
+import { Button, Divider, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger, Text } from "@chakra-ui/react";
+
+export function TestPopover(): JSX.Element {
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Button>Voir compte de tests</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <PopoverArrow />
+        <PopoverCloseButton />
+        <PopoverHeader>Identifiants pour les tests</PopoverHeader>
+        <PopoverBody>
+          <Text>Joueur : </Text>
+          <Text>email: jean.dujardin@mail.io <br />
+            password: yjjk8E676a9JQZ!</Text>
+          <Divider m={3} />
+          <Text>Recruteur : </Text>
+          <Text>email: nicolas.dupon@mail.io <br />
+            password: X346Dc5V7kfYmv!</Text>
+        </PopoverBody>
+      </PopoverContent>
+    </Popover>
+  )
+}
--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,14 +1,17 @@
 import { Flex, Heading, Text } from "@chakra-ui/react";
 import { LoginForm } from "@/components/Form";
 import { LoadingModal } from "@/components/Modal";
+import { Link } from "react-router-dom";
+import { TestPopover } from "@/components/Popover";
 
 export function Login(): JSX.Element {
   return (
     <Flex h="100%" p={10} flexDirection="column" gap="10">
       <Heading as="h1" variant="h1">Connexion à FootFinder</Heading>
       <Text textStyle="secondTitle" textAlign="center"> Explorez un monde d'opportunité sans limite</Text>
+      <TestPopover />
       <LoginForm />
-      <LoadingModal/>
+      <LoadingModal />
     </Flex>
   );
 }
